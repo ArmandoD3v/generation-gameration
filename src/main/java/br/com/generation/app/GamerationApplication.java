82bce0c0f92e67e8da1e55016c78b5bcb618a533
josiasveras <josias_veras@hotmail.com>
2022-01-24 11:10:26 -0300

    @josiasveras [Task: Criar o projeto]

---
+++ src/main/java/br/com/generation/app/GamerationApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerationApplication.class, args);
+	}
+
+}
