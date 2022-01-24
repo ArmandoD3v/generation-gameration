ea43a51ef30615633d5092b50b1f658c588116e8
josiasveras <josias_veras@hotmail.com>
2022-01-24 13:36:40 -0300

    @josiasveras [Task: Comentar classe principal]

--- src/main/java/br/com/generation/app/GamerationApplication.java
+++ src/main/java/br/com/generation/app/GamerationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GamerationApplication {
 
+	// Classe e método principais da aplicação 
 	public static void main(String[] args) {
 		SpringApplication.run(GamerationApplication.class, args);
 	}
